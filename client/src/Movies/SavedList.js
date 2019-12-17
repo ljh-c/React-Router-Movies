51691e34099b7813ca46986d5a70243162637bc3
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-16 19:12:53 -0800

    saved movies link to movie page

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,7 +5,9 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/movies/${movie.id}`} key={movie.title}>
+        <span className="saved-movie" key={movie.title}>{movie.title}</span>
+      </Link>
     ))}
     <Link to="/">
       <div className="home-button">Home</div>
